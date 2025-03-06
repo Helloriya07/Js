17169aed1f845c52b835ac36b30ef29936aa0337
Helloriya07 <missriyashukla@gmail.com>
2025-03-06 09:02:12 +0530

    select.js

--- select.js
+++ select.js
@@ -1,25 +1,30 @@
 console.log("chalo padhai karte hain...")
-
+console.log("selection in js")
 let boxes=document.getElementsByClassName("box")
 console.log(boxes);
 
 boxes[2].style.backgroundColor="blue";
 //or id dekar bhi particular element ko select kr skte hain
 document.getElementById("yellow").style.backgroundColor="yellow";
+
 //inline css using js..it will select first box matches with .box
 document.querySelector(".box").style.backgroundColor="orange";
+
 //agar aap sare box ko select krege
 console.log(document.querySelectorAll(".box"));//it will return a htmlcollection(nodelist)
+
 //entire collection me property set krne k liye loop lgega!!
 document.querySelectorAll(".box").forEach(e=>{
     e.style.backgroundColor="pink";
 })
+
 //using tag name..
 console.log(document.getElementsByTagName("div"))
 
 e=document.getElementsByTagName("div")
 e[4].matches("#redbox")
 // return false bcoz box 4 has no id.
+
 e[6].matches("#yellow");
 //return true
 
